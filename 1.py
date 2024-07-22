3976caa531afee9a9a048b9dd57215f3b92456ef
burgiil <burgiil@yandex.ru>
2024-07-22 20:02:47 +0300

    edit 1.py & add names.txt

--- 1.py
+++ 1.py
@@ -1,1 +1,6 @@
-print ("Avada Kentavra")
+# С помощью оператора with читаем документ и результат отмечаем переменной n 
+with open('names.txt', 'r') as n:
+    # Переменной names запишем то, что прочли из переменной n
+    names = n.read()
+    print(names.splitlines(" "))
+    #
