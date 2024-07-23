7507c6f644adcf4c8420b2cf6b5627913253a4ac
burgiil <burgiil@yandex.ru>
2024-07-23 19:57:25 +0300

    mission completed

--- 1.py
+++ 1.py
@@ -1,6 +1,7 @@
-# С помощью оператора with читаем документ и результат отмечаем переменной n 
+# С помощью оператора with читаем документ и результат отмечаем переменной n и закрываем документ. 
 with open('names.txt', 'r') as n:
     # Переменной names запишем то, что прочли из переменной n
     names = n.read()
-    print(names.splitlines(" "))
-    #
+    r = names.split()
+    names_out = '\n'.join(r)
+    print(names_out)
